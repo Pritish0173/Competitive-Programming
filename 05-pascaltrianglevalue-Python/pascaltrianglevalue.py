832f844bca5a7555d246d1f0f9153452402d29f5
Pritish0173 <pnair.pritish0173@gmail.com>
2021-07-13 11:04:07 +0530

    pascaltrianglevalue solution

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -5,8 +5,21 @@
 # each row starts at column 0. If either row or col 
 # are not legal values, return None, instead of crashing. 
 
+def factorial(n):
+	if(n==1 or n==0):
+		return 1
+	return n*factorial(n-1)
 
-
+def combination(n,r):
+	c = factorial(n)/(factorial(r)*factorial(n-r))
+	return int(c)
 
 def fun_pascaltrianglevalue(row, col):
-	return 1
+    if(col>row):
+        return 0
+    if(col==0 or row==0):
+        return 1
+    n = row
+    r = col
+    value = combination(n,r)
+    return value
