eb5c4649d2723f92f78ed14ec918ee7a58640551
Pritish0173 <pnair.pritish0173@gmail.com>
2021-07-14 10:21:58 +0530

    powersum solution

--- powersum.py
+++ powersum.py
@@ -10,10 +10,29 @@
 # If n is negative, return 0. Similarly, if k is negative, 
 # return 0.
 
+def power(x,y):
+    temp = 0
+    if( y == 0):
+        return 1
+    temp = power(x, int(y / 2))
+    if (y % 2 == 0):
+        return temp * temp
+    else:
+        return x * temp * temp
+
 def powerSum(n, k):
     # Your code goes here...
-    return 0
+    if(n<0 or k<0):
+        return 0
+    Sum = 0
+    for i in range(1,n+1):
+        Sum += power(i,k)
+    return Sum
 
+    
 # Write your own test cases here...
-
+assert powerSum(10, 2)==385,'test case failed'
+assert powerSum(0, 2)==0,'test case failed'
+assert powerSum(10, 0)==10,'test case failed'
+assert powerSum(10, 5)==220825,'test case failed'
 print ("All test cases passed...")
