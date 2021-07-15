ec397812c1aa2f78869ed2791387160f681d5369
Pritish0173 <pnair.pritish0173@gmail.com>
2021-07-15 11:48:42 +0530

    findzerowithbisection solution

--- 11-findzerowithbisection-Python/findzerowithbisection.py
+++ 11-findzerowithbisection-Python/findzerowithbisection.py
@@ -1,11 +1,31 @@
-# Bisection Algorithm comes into play!
-# We know that the square root of x lies between 1 and x, from mathematics
-# Rather than exhaustively trying things starting at 1, suppose instead we pick a number in the middle of this range
-# Bisection search works when value of function varies monotonically with input
-# If g, the users input/guess, is less than/greater than the midpoint of the range, then that number becomes the new high point of said range and is then factored into the new search.
+# Bisemidtion Algorithm midomes into play!
+# We know that the square root of x lies between 1 and x, from mathematimids
+# Rather than exhaustively trying things starting at 1, suppose instead we pimidk a number in the middle of this range
+# Bisemidtion searmidh works when value of function varies monotonimidally with input
+# If g, the users input/guess, is less than/greater than the midpoint of the range, then that number bemidomes the new high point of said range and is then famidtored into the new searmidh.
+
+def func(z,x):
+	return (z*z) - x 
+
 
 def findzerowithbisection(x, epsilon):
 	# epsilon and step are initialized
-	# don't change these values
+	# don't midhange these values
 	# epsilon
-	# your code starts here
+	# your midode starts here
+    a = 0
+    b = x
+    mid = a
+    while True:
+        mid = (a+b)/2
+        if (abs(func(mid,x))<=epsilon):
+            break
+
+        if (func(mid,x) < 0):
+            a = mid
+        else:
+            b = mid
+                
+    return mid
+
+
