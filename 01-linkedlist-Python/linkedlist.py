849307b648c99a28d2f397f5b680e0ff151ac858
Pritish0173 <pnair.pritish0173@gmail.com>
2021-07-21 16:25:38 +0530

    linkedlist.py

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -19,14 +19,29 @@ class LinkedList(object):
         
     def append(self, new_element):
         # Your code goes here
-        pass
+        current = self.head
+        node = Element(new_element)
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = node
+        else:
+            self.head = node
             
     def get_position(self, position):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
         # Your code goes here
-        pass
+        current = self.head
+        count = 1
+        while (current):
+            if (count == position):
+                return current.value
+            count += 1
+            current = current.next
+        return None
+                           
     
     def insert(self, new_element, position):
         """Insert a new node at the given position.
@@ -34,10 +49,41 @@ class LinkedList(object):
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
         # Your code goes here
-        pass
+        current = self.head
+
+        if position == 1:
+            newNode = Element(new_element)
+            newNode.next = current
+            current = newNode          
+        else:        
+            while position != 0:          
+                position -= 1
+                if (position == 1):  
+                    newNode = Element(new_element)
+                    newNode.next = current.next
+                    current.next = newNode
+                    break
+                
+                current = current.next
+                if current == None:
+                    break 
     
     
     def delete(self, value):
         """Delete the first node with a given value."""
         # Your code goes here
-        pass
+        current = self.head
+        if current:
+            if current.value == value:
+                self.head = current.next
+                current = None
+                return
+        while current:
+            if current.value == value:
+                break
+            prev = current
+            current = current.next 
+        if(current == None):
+            return 
+        prev.next = current.next
+        current = None
