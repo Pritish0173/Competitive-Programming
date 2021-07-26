f5431d87947a371bb7f46a4e3e368b94f11abc7c
Pritish0173 <pnair.pritish0173@gmail.com>
2021-07-26 10:39:28 +0530

    recursions_alternatingsum solution

--- 02-recusions_alternatingsum-Python/recursions_alternatingsum.py
+++ 02-recusions_alternatingsum-Python/recursions_alternatingsum.py
@@ -5,4 +5,10 @@
 
 
 def fun_recursions_alternatingsum(l): 
-	return 0
+	if len(l) == 0:
+		return 0
+	if len(l) == 1:
+		return l[0]
+	if len(l) == 2:
+		return l[0] - l[1]
+	return l[0] - l[1] + fun_recursions_alternatingsum(l[2:])
