094c56b64ff1eb60c5736cd6fecdc4ecaf25ff1b
Pritish0173 <pnair.pritish0173@gmail.com>
2021-07-26 15:26:33 +0530

    nondestructiveRemoveRepeats solution

--- non_destructive_remove_repeats.py
+++ non_destructive_remove_repeats.py
@@ -10,7 +10,16 @@
 
 def nondestructiveRemoveRepeats(L):
     # Your code goes here...
-    pass
+    s = set()
+    l = []
+    for i in L:
+        if i in s:
+            continue
+        s.add(i)
+        l.append(i)
+    return l
+
+# Time Complexity - Linear
 
 assert(nondestructiveRemoveRepeats([1, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 3, 5, 2, 7])
 assert(nondestructiveRemoveRepeats([1, 5, 3, 3, 2, 1, 7, 5]) == [1, 5, 3, 2, 7])
