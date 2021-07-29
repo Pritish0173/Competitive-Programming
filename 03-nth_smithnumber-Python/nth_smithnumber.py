0f5744c507fb3af7afef12a1d8f5c6feaa71356c
Pritish0173 <pnair.pritish0173@gmail.com>
2021-07-29 09:30:18 +0530

    nth_smithnumber solution

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -7,6 +7,60 @@
 # so fun_nthsmithnumber(0) should return 4
 # so fun_nthsmithnumber(1) should return 22
 
+def digitsum(n):
+    Sum = 0
+    s = str(n)
+    for i in s:
+        Sum += int(i)
+    return Sum
+
+def isPrime(n):
+    if n<2:
+        return False
+    if n==2:
+        return True
+    if n%2==0:
+        return False
+    maxfactor = round(n**0.5)
+    for i in range(3,maxfactor+1,2):
+        if n%i==0:
+            return False
+    return True
+
+def isComposite(n):
+    if not isPrime(n):
+        return True
+    return False
+
+def primefactors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+def isSmithNumber(n):
+    if not isComposite(n):
+        return False
+    pfl = primefactors(n)
+    l = [digitsum(i) for i in pfl]
+    Sum = sum(l)
+    numsum = digitsum(n)
+    if Sum==numsum:
+        return True
+    return False
 
 def fun_nth_smithnumber(n):
-    return 1
+    found = -1
+    guess = 0
+    while found<n:
+        guess += 1
+        if isSmithNumber(guess):
+            found += 1
+    return guess
